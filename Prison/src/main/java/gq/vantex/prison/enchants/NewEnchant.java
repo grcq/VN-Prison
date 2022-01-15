c5e17ff0a94a7bb4d18a14f5c007ebf7198ff87c
GamerProPlaysYT <66485591+GamerProPlaysYT@users.noreply.github.com>
2022-01-15 18:46:11 +0100

    Enchants

---
+++ Prison/src/main/java/gq/vantex/prison/enchants/NewEnchant.java
@@ -0,0 +1,8 @@
+package gq.vantex.prison.enchants;
+
+import org.bukkit.enchantments.Enchantment;
+
+public abstract class NewEnchant {
+    public abstract String getName();
+    public abstract boolean regularEnchant();
+}
---
+++ Prison/src/main/java/gq/vantex/prison/enchants/enchantment/Efficiency.java
@@ -0,0 +1,16 @@
+package gq.vantex.prison.enchants.enchantment;
+
+import gq.vantex.prison.enchants.NewEnchant;
+
+public class Efficiency extends NewEnchant {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean regularEnchant() {
+        return false;
+    }
+}
